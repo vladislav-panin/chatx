6c274dc042d86214b8562a4e2988b096a92c8876
Vlad Panin <vladislav.o.panin@gmail.com>
2021-04-17 22:00:43 +0300

    разбираюсь с гитом и его branch-ами

--- chat-x/src/main/java/chatx/serv/web/controller/AjaxMsg.java
+++ chat-x/src/main/java/chatx/serv/web/controller/AjaxMsg.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+// разбираюсь с гитом
 @Controller
 public class AjaxMsg
 {
