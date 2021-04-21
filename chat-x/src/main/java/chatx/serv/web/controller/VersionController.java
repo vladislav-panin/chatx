280bc9b413f26eae52516b02a8a9d62bd77c4154
Vlad Panin <vladislav.o.panin@gmail.com>
2021-04-21 19:05:37 +0300

    Добавил html page для версии

--- chat-x/src/main/java/chatx/serv/web/controller/VersionController.java
+++ chat-x/src/main/java/chatx/serv/web/controller/VersionController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
 // *********************************************************************************************************************
@@ -13,9 +14,9 @@ public class VersionController
 {
     @RequestMapping (value = {"/version", "/version/"},  
                      method = RequestMethod.GET,
-                     produces = "application/json;charset=utf-8")   
+                        produces = "text/html;charset=utf-8")
     
-    public JSONObject showPgUserReg (final Model model) 
+    public String showPgUserReg (final Model model) 
     {
         String version = "0.5";
         String date = "2021.04.21"; 
@@ -23,8 +24,10 @@ public class VersionController
         JSONObject jRet = new JSONObject();
         jRet. put ("version", version);
         jRet. put ("date", date);
-         
-        return jRet;
+    
+        String msg = jRet.toJSONString();
+        model.addAttribute("msg", msg);
+        return "version";
     }        
      // ***************************************************************************************************************
 }
